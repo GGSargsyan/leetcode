9af029bf38b83a9f91c603e307ecac35294e34e2
Grigor Sargsyan <grigor.sargsyan@yahoo.com>
2020-04-28 22:49:55 -0700

    Update Search_In_Sorted_Matrix.java

--- Search_In_Sorted_Matrix.java
+++ Search_In_Sorted_Matrix.java
@@ -4,18 +4,18 @@ class Solution {
             return false;
         
         int row = 0;
-		int col = matrix[0].length - 1;
-		while(row < matrix.length && col >= 0){
-			if(matrix[row][col] > target){
-				col--;
-			}
-			else if(matrix[row][col] < target){
-				row++;
-			}
-			else{
-				return true;
-			}
+	int col = matrix[0].length - 1;
+	while(row < matrix.length && col >= 0){
+		if(matrix[row][col] > target){
+			col--;
 		}
-		return false;
+		else if(matrix[row][col] < target){
+			row++;
+		}
+		else{
+			return true;
+		}
+	}
+	return false;
     }
 }
